edd4be062c7b18975f6a12304b4b1c83b21bc3dc
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-07-10 22:35:34 +0530

    header rendered

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import Header from "./Components/Header";
-import Splitter from "./Components/SplitterPage/Splitter";
 
 const App = () => {
   return (
     <div>
-      {/* <Header /> */}
-      <Splitter />
+      <Header />
     </div>
   );
 };
