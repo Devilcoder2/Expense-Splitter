23cb97fac51abe7ec7dabd285bd395e916d490f3
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-07-10 21:18:44 +0530

    Implemented the split button

--- client/src/Components/SplitterPage/NewSplit.jsx
+++ client/src/Components/SplitterPage/NewSplit.jsx
@@ -1,5 +1,11 @@
 const NewSplit = () => {
-  return <div>NewSplit</div>;
+  return (
+    <div className="w-full fixed  bottom-16 left-[1063px]">
+      <div className="">
+        <button className="button-27 ">Split an expense</button>
+      </div>
+    </div>
+  );
 };
 
 export default NewSplit;
--- client/src/Components/SplitterPage/Splitter.jsx
+++ client/src/Components/SplitterPage/Splitter.jsx
@@ -6,8 +6,11 @@ const Splitter = () => {
   return (
     <div className="px-10 bg-gradient-to-r from-pink-400 to-pink-600 text-white h-[100vh]">
       <SplitterHeader />
-      <SplitHistory />
-      <NewSplit />
+
+      <div className="w-[80vw] h-[80vh] bg-gray-100 rounded-lg mx-auto mt-10 relative">
+        <SplitHistory />
+        <NewSplit />
+      </div>
     </div>
   );
 };
