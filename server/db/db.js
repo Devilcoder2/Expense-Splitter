4242779eae2874c6445d0e77d5cb5e16cb8ae1ea
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-07-14 16:49:37 +0530

    Implemented the User Schema

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,27 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/expenseSplitter");
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  groups: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Group",
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
