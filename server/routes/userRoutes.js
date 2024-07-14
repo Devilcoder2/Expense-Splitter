5908c6c0b6bd09011a63e83ed5d8cb8ea8ab5930
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-07-14 17:32:43 +0530

    Listed down all the routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,8 +7,20 @@ router.post("/register", (req, res) => {});
 
 router.post("/login", (req, res) => {});
 
-router.get("/allGroups", (req, res) => {});
+router.get("/allGroupsDebts", (req, res) => {});
 
 router.post("/newGroup", (req, res) => {});
+
+router.get("/myProfile", (req, res) => {});
+
+router.get("/allGroups", (req, res) => {});
+
+router.get("/groupDetails", (req, res) => {});
+
+router.get("/singleGroupDebts", (req, res) => {});
+
+router.get("/allExpenses", (req, res) => {});
+
+router.post("/newExpense", (req, res) => {});
 
 module.exports = router;
