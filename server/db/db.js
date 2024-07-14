e06a9532ddc3e6ee3d3d85f84d4130a38cc85cba
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-07-14 16:50:40 +0530

    Implemented the expenses schema

--- server/db/db.js
+++ server/db/db.js
@@ -50,10 +50,58 @@ const groupsSchema = new mongoose.Schema({
   ],
 });
 
+const userInExpenseSchema = new mongoose.Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  amountToPay: {
+    type: Number,
+    required: true,
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const singleExpensesSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  splittedBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  dateOfSplit: {
+    type: Date,
+    default: Date.now,
+  },
+  userInExpsense: [userInExpenseSchema],
+});
+
+const expensesSchema = new mongoose.Schema({
+  groupId: {
+    type: Schema.Types.ObjectId,
+    ref: "Group",
+    required: true,
+  },
+  singleExpenses: [singleExpensesSchema],
+});
+
 const User = mongoose.model("User", userSchema);
 const Group = mongoose.model("Group", groupsSchema);
+const Expense = mongoose.model("Expense", expensesSchema);
 
 module.exports = {
   User,
   Group,
+  Expense,
 };
